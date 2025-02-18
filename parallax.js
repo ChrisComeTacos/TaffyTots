1f521cf69a050741700119c59c5d4f22eef0d79d
ChrisComeTacos <leanyuwu@gmail.com>
2025-02-18 01:19:30 -0600

    Se sigue con el diseno

--- parallax.js
+++ parallax.js
@@ -2,6 +2,8 @@ let Taffy = document.getElementById("Taffy");
 // let Arbusto = document.getElementById("Arbusto");
 let Petalos = document.getElementById("Petalos");
 let Arboles = document.getElementById("Arboles");
+let Nombre = document.getElementById("Nombre");
+
 // let Fondo = document.getElementById("Fondo");
 
 window.addEventListener("scroll", () => {
@@ -11,5 +13,6 @@ window.addEventListener("scroll", () => {
   // Arbusto.style.marginTop = value * 0.7 + "px";
   Petalos.style.marginTop = value * 1.8 + "px";
   Arboles.style.marginTop = value * 1.6 + "px";
+  Nombre.style.marginTop = value * -1.5 + "px";
   // Fondo.style.marginBottom = value * 0.2 + "px";
 });
