d98098e47ae6bfd360e63b03bef9085c0bf666c0
ChrisComeTacos <leanyuwu@gmail.com>
2025-02-17 22:18:02 -0600

    Se empieza el proyecto

---
+++ parallax.js
@@ -0,0 +1,15 @@
+let Taffy = document.getElementById("Taffy");
+// let Arbusto = document.getElementById("Arbusto");
+let Petalos = document.getElementById("Petalos");
+let Arboles = document.getElementById("Arboles");
+// let Fondo = document.getElementById("Fondo");
+
+window.addEventListener("scroll", () => {
+  let value = window.scrollY;
+
+  Taffy.style.marginTop = value * 1.5 + "px";
+  // Arbusto.style.marginTop = value * 0.7 + "px";
+  Petalos.style.marginTop = value * 1.8 + "px";
+  Arboles.style.marginTop = value * 1.6 + "px";
+  // Fondo.style.marginBottom = value * 0.2 + "px";
+});
